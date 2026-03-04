1280684e6594a77b87a56c130178c023a4019564
Peter Heri <peterheri@Peters-MacBook-Pro.local>
2026-03-04 14:01:35 +0300

    Addlogout route

--- src/pages/Admin/AdminInvestments.jsx
+++ src/pages/Admin/AdminInvestments.jsx
@@ -11,7 +11,7 @@ export default function AdminInvestmentsPage() {
     const fetchInvestmentsBusinesses = async () => {
       try {
         const { data } = await axios.get(
-          "https://bizz-mall-backend-production.up.railway.app/api/business/investments",
+          "https://bizz-mall-backend-production.up.railway.app/api/business/is_investment=true",
           {
             withCredentials: true,
           },
